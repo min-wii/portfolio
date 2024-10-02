67ccf86decf40dbaf7dd8a62c16ecc92fc7fb13d
unknown <zooklimk90@gmail.com>
2024-10-02 17:55:12 +0900

    About Me:컨텐츠 작성

--- assets/js/dist/custom.dev.js
+++ assets/js/dist/custom.dev.js
@@ -1,36 +1,9 @@
 "use strict";
 
-// const navBtns = document.querySelectorAll('header nav ul li button');
-// const sections = document.querySelectorAll('main section');
-// const resetBtn = document.querySelector('.frame');
-// resetBtn.addEventListener('click',(e)=>{
-//     navBtns.forEach((btn)=>{
-//         btn.classList.remove('active')
-//     })
-//     sections.forEach((sec)=>{
-//         sec.classList.remove('active')
-//     })
-// })
-// navBtns.forEach((btn, indx) => {
-//     btn.addEventListener('click', (e) => {
-//         // 모든 버튼에서 'active' 클래스 제거
-//         navBtns.forEach(btn => btn.classList.remove('active'));
-//         // 현재 클릭된 버튼에만 'active' 클래스 추가
-//         e.target.classList.add('active');
-//         // 섹션을 처리
-//         sections.forEach((sec, secIndx) => {
-//             if (secIndx === indx) {
-//                 sec.classList.add('active');
-//             } else {
-//                 sec.classList.remove('active');
-//             }
-//         });
-//     });
-// });
-// 요소 선택
 var navBtns = document.querySelectorAll('header nav ul li button');
 var sections = document.querySelectorAll('main section');
-var resetBtn = document.querySelector('.frame'); // 모든 버튼 및 섹션에서 'active' 클래스 제거
+var tagBtns = document.querySelectorAll('.sec__tag li');
+var resetBtn = document.querySelector('.frame');
 
 var resetActiveClasses = function resetActiveClasses() {
   navBtns.forEach(function (btn) {
@@ -38,23 +11,20 @@ var resetActiveClasses = function resetActiveClasses() {
   });
   sections.forEach(function (sec) {
     return sec.classList.remove('active');
-  });
-}; // 버튼 클릭 시 동작
-
+  }); // tagBtns.forEach(btn => btn.classList.remove('active'));
+};
 
 var handleNavButtonClick = function handleNavButtonClick(indx) {
-  resetActiveClasses(); // 모든 'active' 클래스 제거
+  resetActiveClasses();
+  navBtns[indx].classList.add('active');
+  sections[indx].classList.add('active'); // tagBtns[indx].classList.add('active');
+};
 
-  navBtns[indx].classList.add('active'); // 클릭된 버튼에 'active' 클래스 추가
-
-  sections[indx].classList.add('active'); // 해당 섹션에 'active' 클래스 추가
-}; // 초기화 버튼 클릭 이벤트
-
-
-resetBtn.addEventListener('click', resetActiveClasses); // 내비게이션 버튼 클릭 이벤트 추가
-
+resetBtn.addEventListener('click', function () {
+  resetActiveClasses();
+});
 navBtns.forEach(function (btn, indx) {
   btn.addEventListener('click', function () {
-    return handleNavButtonClick(indx);
+    handleNavButtonClick(indx); // 그 후 클릭된 버튼에 맞는 섹션을 활성화
   });
 });
