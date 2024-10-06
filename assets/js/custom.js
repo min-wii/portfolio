ea6a87c52fc0341a81addcafc9e9bb85682ace60
Ginger <118656791+min-wii@users.noreply.github.com>
2024-10-06 22:02:24 +0900

    projects 내용 정리 필요

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,12 @@
+// 로딩 페이지
 window.addEventListener("load", function() {
     setTimeout(function() {
-      document.getElementById("loader").style.display = "none";
-      document.getElementById("content").style.display = "block";
-    }, 5000);  // 3000ms = 3초
+      document.getElementById("loader").style.zIndex = "-100";
+      document.getElementById("content").style.display = "flex";
+    }, 1700);  
   });
 
+// 전체 
 const navBtns = document.querySelectorAll('header nav ul li button');
 const sections = document.querySelectorAll('main section');
 const tagBtns = document.querySelectorAll('.sec__tag li');
@@ -14,7 +16,6 @@ const resetBtn = document.querySelector('.frame');
 const resetActiveClasses = () => {
     navBtns.forEach(btn => btn.classList.remove('active'));
     sections.forEach(sec => sec.classList.remove('active'));
-    // tagBtns.forEach(btn => btn.classList.remove('active'));
 };
 
 
@@ -22,7 +23,6 @@ const handleNavButtonClick = (indx) => {
     resetActiveClasses();
     navBtns[indx].classList.add('active');
     sections[indx].classList.add('active'); 
-    // tagBtns[indx].classList.add('active');
 };
 
 
@@ -32,7 +32,49 @@ resetBtn.addEventListener('click', () => {
 
 navBtns.forEach((btn, indx) => {
     btn.addEventListener('click', () => {
-        handleNavButtonClick(indx); // 그 후 클릭된 버튼에 맞는 섹션을 활성화
+        handleNavButtonClick(indx); 
     });
 });
+
+// 프로젝트 차트
+google.charts.load('current', { packages: ['corechart', 'bar'] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Skills');
+    data.addColumn('number', 'Proficiency (%)');
+    data.addRows([
+        ['HTML', 100],
+        ['CSS', 90],
+        ['SCSS', 90],
+        ['JS', 80],
+        ['jQuery', 70],
+        ['React', 50],
+        ['Vue', 50],
+    ]);
+
+    var options = {
+        legend: { position: 'none' },
+        hAxis: {
+            minValue: 0,
+            maxValue: 100,
+        },
+        bar: { groupWidth: "90%" },
+        colors: ['#4B7F93'],
+    };
+
+    // 차트를 그릴 div 요소의 크기를 가져옴
+    var chartContainer = document.getElementById('skillsChart');
+    var containerWidth = chartContainer.clientWidth; 
+    var containerHeight = chartContainer.clientHeight; 
+
+    // 옵션에 부모 div의 크기를 설정
+    options.width = containerWidth; 
+    options.height = containerHeight; 
+    var chart = new google.charts.Bar(chartContainer);
+    chart.draw(data, google.charts.Bar.convertOptions(options));
+}
+
+window.addEventListener('resize', drawChart);
 
--- assets/js/dist/custom.dev.js
+++ assets/js/dist/custom.dev.js
@@ -1,11 +1,13 @@
 "use strict";
 
+// 로딩 페이지
 window.addEventListener("load", function () {
   setTimeout(function () {
-    document.getElementById("loader").style.display = "none";
+    document.getElementById("loader").style.zIndex = "-100";
     document.getElementById("content").style.display = "flex";
-  }, 5000); // 3000ms = 3초
-});
+  }, 1700);
+}); // 전체 
+
 var navBtns = document.querySelectorAll('header nav ul li button');
 var sections = document.querySelectorAll('main section');
 var tagBtns = document.querySelectorAll('.sec__tag li');
@@ -17,13 +19,13 @@ var resetActiveClasses = function resetActiveClasses() {
   });
   sections.forEach(function (sec) {
     return sec.classList.remove('active');
-  }); // tagBtns.forEach(btn => btn.classList.remove('active'));
+  });
 };
 
 var handleNavButtonClick = function handleNavButtonClick(indx) {
   resetActiveClasses();
   navBtns[indx].classList.add('active');
-  sections[indx].classList.add('active'); // tagBtns[indx].classList.add('active');
+  sections[indx].classList.add('active');
 };
 
 resetBtn.addEventListener('click', function () {
@@ -31,6 +33,42 @@ resetBtn.addEventListener('click', function () {
 });
 navBtns.forEach(function (btn, indx) {
   btn.addEventListener('click', function () {
-    handleNavButtonClick(indx); // 그 후 클릭된 버튼에 맞는 섹션을 활성화
+    handleNavButtonClick(indx);
   });
+}); // 프로젝트 차트
+
+google.charts.load('current', {
+  packages: ['corechart', 'bar']
 });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Skills');
+  data.addColumn('number', 'Proficiency (%)');
+  data.addRows([['HTML', 100], ['CSS', 90], ['SCSS', 90], ['JS', 80], ['jQuery', 70], ['React', 50], ['Vue', 50]]);
+  var options = {
+    legend: {
+      position: 'none'
+    },
+    hAxis: {
+      minValue: 0,
+      maxValue: 100
+    },
+    bar: {
+      groupWidth: "90%"
+    },
+    colors: ['#4B7F93']
+  }; // 차트를 그릴 div 요소의 크기를 가져옴
+
+  var chartContainer = document.getElementById('skillsChart');
+  var containerWidth = chartContainer.clientWidth;
+  var containerHeight = chartContainer.clientHeight; // 옵션에 부모 div의 크기를 설정
+
+  options.width = containerWidth;
+  options.height = containerHeight;
+  var chart = new google.charts.Bar(chartContainer);
+  chart.draw(data, google.charts.Bar.convertOptions(options));
+}
+
+window.addEventListener('resize', drawChart);
